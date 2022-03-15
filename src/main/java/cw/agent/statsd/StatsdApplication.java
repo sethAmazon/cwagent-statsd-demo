8299b0c64e1d7bcb402bb11f112fa3f178e77f8e
Seth Levine <sethlevi@amazon.com>
2022-03-15 14:04:10 -0400

    Add More Tags

--- src/main/java/cw/agent/statsd/StatsdApplication.java
+++ src/main/java/cw/agent/statsd/StatsdApplication.java
@@ -13,8 +13,24 @@ public class StatsdApplication {
 				.build();
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Sending seth-statd-metric");
-			client.gauge("seth-statd-metric", 101 * i, "Seth0:Seth0");
-			Thread.sleep(60000);
+			client.gauge("seth-statd-metric", 101 * i,
+					"Seth0:Seth0",
+					"S1:S1",
+					"2:2",
+					"3:2",
+					"4:2",
+					"5:2",
+					"6:2",
+					"7:2",
+					"8:2",
+					"9:2",
+					"10:2",
+					"11:2",
+					"12:2",
+					"13:2",
+					"14:2",
+					"15:2"
+			);
 		}
 		client.close();
 	}

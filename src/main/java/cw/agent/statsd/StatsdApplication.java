bb12b8f52acc959bc91cbe5fb43152a4fef314e0
Seth Levine <sethlevi@amazon.com>
2022-03-15 13:45:24 -0400

    Sample Commit

---
+++ src/main/java/cw/agent/statsd/StatsdApplication.java
@@ -0,0 +1,21 @@
+package cw.agent.statsd;
+
+import com.timgroup.statsd.NonBlockingStatsDClientBuilder;
+import com.timgroup.statsd.StatsDClient;
+
+public class StatsdApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		StatsDClient client = new NonBlockingStatsDClientBuilder()
+				.prefix("statsd")
+				.hostname("localhost")
+				.port(8125)
+				.build();
+		for (int i = 0; i < 10; i++) {
+			client.gauge("seth-statd-metric", 101 * i, "Seth0:Seth0");
+			Thread.sleep(60000);
+		}
+		client.close();
+	}
+
+}
---
+++ src/test/java/cw/agent/statsd/StatsdApplicationTests.java
@@ -0,0 +1,13 @@
+package cw.agent.statsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

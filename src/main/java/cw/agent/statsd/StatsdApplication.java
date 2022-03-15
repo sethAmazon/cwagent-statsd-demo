407bece2e58ed5e76a1bcf164970879a3cc34698
Seth Levine <sethlevi@amazon.com>
2022-03-15 14:19:30 -0400

    Loop 5

--- src/main/java/cw/agent/statsd/StatsdApplication.java
+++ src/main/java/cw/agent/statsd/StatsdApplication.java
@@ -11,7 +11,7 @@ public class StatsdApplication {
 				.hostname("localhost")
 				.port(8125)
 				.build();
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 5; i++) {
 			System.out.println("Sending seth-statd-metric");
 			client.gauge("seth-statd-metric", 101 * i,
 					"Seth0:Seth0",
